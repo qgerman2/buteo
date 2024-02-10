a1baf075e24f8b4495caf7b1a26b24f8068644a9
qgerman2 <qgerman2@gmail.com>
2024-02-10 19:48:09 -0300

    increible

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,53 @@
 use async_tungstenite::tokio::TokioAdapter;
 use async_tungstenite::{accept_async, tungstenite::Message};
-use futures::{SinkExt, StreamExt};
+use futures::{Future, SinkExt, StreamExt};
 use serde::{Deserialize, Serialize};
+use std::pin::Pin;
+use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll};
 use std::{io, net::SocketAddr, str::FromStr};
+use strum_macros::EnumDiscriminants;
 use tokio::net::{TcpListener, TcpStream};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, EnumDiscriminants)]
+#[strum_discriminants(name(MessageType))]
 enum CustomMessage {
     Hola { x: String, y: String },
     Chao(i32, i32),
+}
+
+type ListOfWakers = Vec<(
+    MessageType,
+    Box<dyn Fn(CustomMessage) + Sync + Send + 'static>,
+)>;
+
+struct MyFuture {
+    t: MessageType,
+    list: Arc<Mutex<ListOfWakers>>,
+    val: Arc<Mutex<Option<CustomMessage>>>,
+}
+
+impl Future for MyFuture {
+    type Output = CustomMessage;
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.val.lock().unwrap().as_ref() {
+            Some(msg) => Poll::Ready(msg.clone()),
+            None => {
+                println!("ESTOY EN EL FUTURO");
+                let cx2 = cx.waker().clone();
+                let val2 = self.val.clone();
+                self.list.lock().unwrap().push((
+                    self.t,
+                    Box::new(move |msg: CustomMessage| {
+                        println!("se ha llamado el closure holy shi");
+                        val2.lock().unwrap().replace(msg);
+                        cx2.wake_by_ref();
+                    }),
+                ));
+                Poll::Pending
+            }
+        }
+    }
 }
 
 #[tokio::main]
@@ -27,6 +66,8 @@ async fn connection(stream: Result<(TcpStream, SocketAddr), io::Error>) {
         return;
     };
     println!("Conectado {}", addr);
+
+    let msgs = Arc::new(Mutex::new(ListOfWakers::new()));
     loop {
         let Some(Ok(msg)) = stream.next().await else {
             println!("no se recibio nada");
@@ -40,18 +81,46 @@ async fn connection(stream: Result<(TcpStream, SocketAddr), io::Error>) {
         if let Message::Text(tex) = msg {
             if let Ok(jj) = serde_json::from_str::<CustomMessage>(&tex.to_string()) {
                 println!("got json {:?}", jj);
+                on_message(msgs.clone(), jj);
             } else {
                 println!("error decodificando el json");
             }
         }
-        let mimensaje = CustomMessage::Hola {
-            x: "miau".into(),
-            y: "miu".into(),
-        };
-        let msg = Message::Text(serde_json::to_string(&mimensaje).unwrap());
-        if let Err(e) = stream.send(msg).await {
-            println!("error {:}", e);
-            return;
+    }
+}
+
+fn on_message(msgs: Arc<Mutex<ListOfWakers>>, msg: CustomMessage) {
+    for i in msgs.lock().unwrap().iter() {
+        if i.0 == MessageType::from(&msg) {
+            println!("encontrado que se esta esperando un chao!!!!");
+            i.1.as_ref()(msg.clone());
         }
     }
+    msgs.lock()
+        .unwrap()
+        .retain(|i| i.0 != MessageType::from(&msg));
+    match msg {
+        CustomMessage::Hola { .. } => on_hola(msgs),
+        CustomMessage::Chao(..) => println!("chao"),
+    }
+}
+
+fn on_hola(msgs: Arc<Mutex<ListOfWakers>>) {
+    println!("hola");
+    tokio::spawn(wait_for_chao(msgs));
+}
+
+async fn wait_for(t: MessageType, msgs: Arc<Mutex<ListOfWakers>>) -> CustomMessage {
+    let aver = MyFuture {
+        t,
+        list: msgs,
+        val: Arc::new(Mutex::new(None)),
+    };
+    aver.await
+}
+
+async fn wait_for_chao(msgs: Arc<Mutex<ListOfWakers>>) {
+    println!("waiting for chao");
+    let fuutuuro = wait_for(MessageType::Chao, msgs).await;
+    println!("TERMINO EL AWAIT")
 }
